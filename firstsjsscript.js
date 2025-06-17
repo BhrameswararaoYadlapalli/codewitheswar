58f0e45f2062a1a175fa60b0e6c81c7e2e9d8562
BhrameswararaoYadlapalli <76121967+BhrameswararaoYadlapalli@users.noreply.github.com>
2025-06-17 12:42:35 -0400

    initial commit

---
+++ firstsjsscript.js
@@ -0,0 +1,88 @@
+// This is a simple JavaScript script that greets the user by name
+// var name = "Nani";
+// console.log("Hello " + name + "!");
+//  let greeting = "Welcome to the JavaScript world!";
+//  console.log(greeting);
+
+//  function greetUser(userName) {
+//      return "Hello, " + userName + "!";
+//  }
+//  console.log(greetUser("Nani"))
+//  ;
+// function addNumbers(a, b) {
+//     return a + b;
+// }
+// console.log(addNumbers(5, 10));
+// function loopExample() {
+//     for (let i = 0; i < 5; i++) {
+//         console.log("Iteration: " + i);
+//     }
+// }
+
+// loopExample();
+// function checkNumber() {
+//     let number = 10;
+//     if (number > 0) {
+//         console.log("The number is positive.");
+//     } else if (number < 0) {
+//         console.log("The number is negative.");
+//     } else {
+//         console.log("The number is zero.");
+//     }
+    
+// }
+// checkNumber();
+// let fruits = ["apple", "banana", "cherry"];
+// // function addFruit(fruit) {
+// //     for (let i = 0; i < fruits.length; i++)
+// //     console.log("Fruit: " + fruits[i]);
+// // };
+// // addFruit();
+// console.log(fruits);
+// fruits.push("orange");
+// console.log(fruits);
+// fruits.pop();
+// console.log(fruits);
+// fruits.shift();
+// console.log(fruits);
+// fruits.unshift("grape");
+// console.log(fruits);
+// console.log(fruits.includes('banana'));
+
+// const hobbies={
+//     hobby1: "reading",
+//     hobby2: "gaming",
+//     hobby3: "hiking"
+// }
+// // console.log(hobbies);
+
+// console.log(hobbies.hobby1);
+
+// const human = {
+//    Person:{
+//     firstName: "Nani",
+//     lastName: "Doe",
+//     age: 27,
+//     },
+//     greet: function() {
+//         console.log("Hello, my name is " + human.Person.firstName);
+//         console.log("I am " + human.Person.age + " years old.");
+//     }
+
+// };
+// human.greet();
+
+// function car(name) {
+//     return "Car name is " + name; 
+// }
+// console.log(car("Toyota"));
+ 
+// const car= name => "Car name is " + name;  
+// console.log(car("Toyota"));
+
+// Manipulating the HTML and CSS using JavaScript
+
+const Heading1= document.querySelector(".heading");
+Heading1.textContent = "Welcome to JavaScript Manipulation!";
+Heading1.style.fontSize = "30px";
+Heading1.style.color = "blue";
